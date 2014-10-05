86b4874cc074f5b302d006726ce5768eebdb0650
yuranisalamanca <yuranisalamanca@gmail.com>
2014-10-04 22:50:49 -0500

    Vista propuesta

---
+++ Codigo-fuente/Colciencias/application/views/barra.php
@@ -0,0 +1,34 @@
+<center><img style="width: 73.2%" src="img/logo-colciencias.jpg"></center>
+<div class="row">
+    
+<nav class="top-bar" data-topbar style="background-color: #086A87;">
+  <ul class="title-area">
+    <li class="name">
+      <h1><a href="index.php"><b>Colciencias</b></a></h1>
+    </li>
+    
+  </ul>
+
+  <section class="top-bar-section">
+    <ul class="right">
+      <li class="has-dropdown">
+        <a href="#" style="background-color: #086A87;" >Registro</a>
+        <ul class="dropdown">
+          <li ><a href="ingresarUsuario.php" style="background-color: #086A87;">Crear una cuenta</a></li>
+          <li ><a href="login.php" style="background-color:#086A87 ;">Iniciar sesion</a></li>
+        </ul>
+      </li>
+    </ul>
+
+    
+    <ul class="left">
+      <li><a href="#"style="background-color: #086A87;">Convocatoria</a></li>
+      <li><a href="#"style="background-color: #086A87;">Evaluador</a></li>
+      <li><a href="#"style="background-color: #086A87;">Propuesta</a></li>
+      <li><a href="#"style="background-color: #086A87;">Panel</a></li>
+
+    </ul>
+  </section>
+</nav>
+
+</div>
---
+++ Codigo-fuente/Colciencias/application/views/homeUsuario.php
@@ -0,0 +1,54 @@
+
+<!doctype html>
+<html class="no-js" lang="en">
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Colciencias</title>
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>css/foundation.css">
+    <script src="js/modernizr.js"></script>
+</head>
+<body>
+
+	 <br>
+        <div class="row">
+         <center>
+         	<ul class="example-orbit" data-orbit>
+			  <li>
+			    <img src="img/imagen1home.jpg" alt="slide 1" />
+			    <div class="orbit-caption">
+			      
+			    </div>
+			  </li>
+			  <li class="active">
+			    <img src="img/imagen2home.jpg" alt="slide 2" />
+			    <div class="orbit-caption">
+			      
+			    </div>
+			  </li>
+			  <li>
+			    <img src="img/imagen3home.jpg" alt="slide 3" />
+			    <div class="orbit-caption">
+			     
+			    </div>
+			  </li>
+			</ul>
+		
+         </center>
+        </div>
+        <br>
+        <center><h3 class="subheader">Todos los derechos reservados</h3><center>
+         <center><h3 class="subheader">2014</h3><center>
+       
+         
+        <script src="<?php echo base_url(); ?>js/foundation/foundation.js"></script>
+        <script src="<?php echo base_url(); ?>js/foundation.min.js"></script>
+        <script src="<?php echo base_url(); ?>js/foundation/foundation.topbar.js"></script>
+        <script src="<?php echo base_url(); ?>js/foundation/foundation.abide.js"></script>
+        <script src="<?php echo base_url(); ?>js/vendor/jquery.js"></script>
+        
+        <script>
+        $(document).foundation();
+        </script>
+</body>
+</html>
