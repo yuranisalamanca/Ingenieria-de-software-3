b2425cfe79ffcf87e698cdda2d84bf6b2a25165c
Karen Ramirez <karendarm12@gmail.com>
2014-11-05 03:54:44 -0500

    Se agregaron pruebas unitarias

--- Codigo-fuente/Colciencias/application/controllers/evaluadorTest.php
+++ Codigo-fuente/Colciencias/application/controllers/evaluadorTest.php
@@ -2,11 +2,6 @@
 
 Class EvaluadorTest extends CI_Controller {
 
-	  function __construct(){
-        parent::__construct();
-        $this->load->database();
-  }
-
 	
 	public function test()
 	{
@@ -15,12 +10,14 @@ Class EvaluadorTest extends CI_Controller {
 
 		$this->unit->run($this->evaluadores_model->listarEvaluadores(),'is_array','Listar evaluadores test');
 		$this->unit->run($this->evaluadores_model->listar3EvaluadoresPorPropuesta(1),'is_array','listar3EvaluadoresPorPropuesta test','Se probo que la propuesta retorna un array de 3 evaluadores');
-		$this->unit->run($this->evaluadores_model->listaDeEvaluadoresYPropuestas(1),'is_null','Lista de evaluadores y propuestas por convocatoria', 'Se probo que la funcion listaDeEvaluadoresYPropuestas retorne un array');
-		$this->unit->run($this->evaluadores_model->listaDeEvaluadoresYPropuestas(1),'is_null','Lista de evaluadores y propuestas por convocatoria', 'Se probo que la funcion listaDeEvaluadoresYPropuestas retorne un array');
-		$this->unit->run($this->evaluadores_model->listaDeEvaluadoresYPropuestasOrdenado(1,1,2),'is_null','Lista de evaluadores y propuestas por convocatoria ordenadas', 'Se probo que la funcion listaDeEvaluadoresYPropuestasOrdenado retorne un array');
-		$this->unit->run($this->evaluadores_model->listaDeEvaluadoresYPropuestasOrdenado(1,1,2),'is_null','Lista de evaluadores y propuestas por convocatoria ordenadas', 'Se probo que la funcion listaDeEvaluadoresYPropuestasOrdenado retorne un array');
-		$this->unit->run($this->evaluadores_model->eliminarEvaluador(4,45),'is_null','eliminarEvaluador test');
-		$this->bd->trans_rollback();
+		$this->unit->run($this->evaluadores_model->listaDeEvaluadoresYPropuestas(1),'is_array','Lista de evaluadores y propuestas por convocatoria', 'Se probo que la funcion listaDeEvaluadoresYPropuestas retorne un array');
+		$this->unit->run($this->evaluadores_model->listaDeEvaluadoresYPropuestasOrdenado(1,1,2),'is_array','Lista de evaluadores y propuestas por convocatoria ordenadas', 'Se probo que la funcion listaDeEvaluadoresYPropuestasOrdenado retorne un array');
+		//$this->unit->run($this->evaluadores_model->eliminarEvaluador(4,45),'is_null','eliminarEvaluador test');
+		$this->unit->run($this->evaluadores_model->getEvaluador(1),'is_array','Obtener un evaluador','Se probo que la funcion getEvaluador retorna un string');
+		$this->unit->run($this->evaluadores_model->listarEvaluadoresPorConvocatoria(1),'is_array','Listar evaluadores por convocatoria','');
+		$this->unit->run($this->evaluadores_model->listarEvaluadoresPorConvocatoria(1),'is_array','buscar evaluadores por convocatoria','');
+		//$this->unit->run($this->evaluadores_model->marcarEvaluadorSuplente(2,1),'is_array','Marcar un evaluador como suplente','');
+		$this->unit->run($this->evaluadores_model->buscarEvaluadoresYPropuestasPorConvocatoria(1),'is_bool','Busqueda de evaluadores y propuestas por convocatoria');
 		echo $this->unit->report();	
 	}
 
