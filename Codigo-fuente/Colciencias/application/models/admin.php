36d7d0848ce075cb52e4fbbd4776e50ef963c656
Karen Ramirez <karendarm12@gmail.com>
2014-11-09 21:09:01 -0500

    se agrego cambio

--- Codigo-fuente/Colciencias/application/models/admin.php
+++ Codigo-fuente/Colciencias/application/models/admin.php
@@ -1,4 +1,4 @@
-<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+<?php
 
 ///////////////////////////////////////////////////////////////////////////
 //	Esta clase es la responsable de controlar comunicar la vista con el modelo
